7647810904de6a0009765f1373168733e8b95099
sai madhan <saimadhan@outlook.in>
2023-06-20 19:42:55 +0530

    skeleton with tailwind

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,9 @@
+import { Account, Avatars, Client } from "appwrite";
+
+export const client = new Client()
+  .setEndpoint(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}`)
+  .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`);
+
+export const account = new Account(client);
+
+export const avatars = new Avatars(client);
