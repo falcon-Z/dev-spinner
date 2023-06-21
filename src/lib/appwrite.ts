8e8637d098f98ab5a53d8efec8fd085316f03ae8
sai madhan <saimadhan@outlook.in>
2023-06-21 19:06:15 +0530

    setup appwrite

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,8 +1,10 @@
 import { Account, Avatars, Client } from "appwrite";
 
+import { PUBLIC_APPWRITE_PROJECT_ID, PUBLIC_APPWRITE_ENDPOINT } from '$env/static/public';
+
 export const client = new Client()
-  .setEndpoint(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}`)
-  .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`);
+  .setEndpoint(`${PUBLIC_APPWRITE_ENDPOINT}`)
+  .setProject(`${PUBLIC_APPWRITE_PROJECT_ID}`);
 
 export const account = new Account(client);
 
